99c122b6687ddb6cba739e4cd65388784ca0cede
KnightRadiant44 <mahir.islam.numayeer@gmail.com>
2022-07-09 10:10:26 +0600

    initial commit - my first bot

---
+++ my_first_bot/file.py
@@ -0,0 +1,40 @@
+import discord
+import random
+
+TOKEN = 'OTk1MDA0NTM1MTc5MzE3NDIw.GqK50H._vukyaIL7ckBZkh_x0H6_9jQl0STfqpuEO_mY0'
+
+'''Connect to the client and make the bot come online'''
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+''''''
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user: #making sure the bot doesn't respond to itself
+        return
+    
+    if message.channel.name == 'hometown-of-silence': #making sure that the bot responds ONLY in a certain channel. This is to prevent the bot from responding everywhere. this is also for the bot to start chatting
+        if user_message.lower() == "hello": #notice the .lower() -> this is necessary in order for the bot to recognize the message. Bots only recognize LOWERCASE messages.
+            await message.channel.send(f'Hello {username}!')
+            return
+        if user_message.lower() == "bye": #cue for the bot to stop chatting
+            await message.channel.send(f'See you later {username}!')
+            return
+        if user_message.lower() == "!random": #any random response for the bot
+            response = f'This is your random number: {random.randrange(10000000)}'
+            await message.channel.send(response)
+            return
+    if user_message.lower() == "!anywhere":
+        await message.channel.send('This can be used anywhere!')
+        return
+
+client.run(TOKEN)
